93ff1e11469f95158eff220beae33cb982d5c2fb
FredrickSimi <fkemosi@gmail.com>
2021-06-17 12:30:47 +0300

    second commit

---
+++ README.md
@@ -0,0 +1,18 @@
+### JavaScript File Encrypter
+
+#### Clone this repository into your computer
+
+    $ git clone https://github.com/fredricksimi/mycrypter.git
+
+#### cd into the project directory and create a virtual environment
+    $ cd mycrypter
+    $ python3 -m venv myvenv
+
+#### Activate the virtual environment
+    $ source myvenv/bin/activate
+
+#### Proceed to install the Python packages needed
+    $ pip3 install -r requirements.txt
+
+#### Start the server from the terminal
+    $ python3 manage.py runserver
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,26 +1,4 @@
 from django.shortcuts import render
-from cryptography.fernet import Fernet
 
-import hashlib
-from Crypto import Random
-from Crypto.Cipher import AES
-from base64 import b64encode, b64decode
-
-class AESCipher(object):
-    def __init__(self, key):
-        self.block_size = AES.block_size
-        self.key = hashlib.sha256(key.encode()).digest()
-
-
-# What do you want to do? Encrypt or Decrypt?
 def home_view(request):
     return render(request, 'mainapp/index.html')
-
-# Choose which file to encrypt/decrypt
-def encdec(request):
-    return render(request, 'mainapp/encdec.html')
-
-# Enter a pass phrase that will be used as your encryption/decryption key
-def pass_phrase(request):
-    return render(request, 'mainapp/pass_phrase.html')
-
